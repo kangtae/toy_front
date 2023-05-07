e57608c9398c01c3db2fcbaa6a91fea25997d9c2
kangtaewan <bras1717@naver.com>
2023-05-07 22:38:10 +0900

    nav 링크 추가

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,47 @@
+'use client'
+
 import Link from "next/link";
+import HomeIcon from "@/components/ui/icons/HomeIcon";
+import HomeFillIcon from "@/components/ui/icons/HomeFillIcon";
+import SearchIcon from "@/components/ui/icons/SearchIcon";
+import SearchFillIcon from "@/components/ui/icons/SearchFillIcon";
+import NewIcon from "@/components/ui/icons/NewIcon";
+import NewFillIcon from "@/components/ui/icons/NewFillIcon";
+import {usePathname} from "next/navigation";
+
 
 const menu = [
 	{
-		href : '/', icon: <AiOutlineHome />
+		href : '/',
+		icon: <HomeIcon />,
+		clickedIcon: <HomeFillIcon />
+	},
+	{
+		href: "/search",
+		icon: <SearchIcon />,
+		clickedIcon: <SearchFillIcon />
+	},
+	{
+		href: "/new",
+		icon: <NewIcon />,
+		clickedIcon: <NewFillIcon />
 	}
 ]
 export default function Navbar(props) {
+	const pathName = usePathname()
 	return <div>
 		<Link href="/">
-			<h1>Instargram</h1>
+			<h1>Instantgram</h1>
 		</Link>
 		<nav>
 			<ul>
-
+				{
+					menu.map(item => <li key={item.href}>
+						<Link href={item.href}>
+							{pathName === item.href ? item.clickedIcon : item.icon}
+						</Link>
+					</li>)
+				}
 			</ul>
 		</nav>
 	</div>;
---
+++ src/components/ui/icons/NewFillIcon.tsx
@@ -0,0 +1,4 @@
+import {BsFillPlusSquareFill} from "react-icons/all";
+export default function NewFillIcon() {
+	return <BsFillPlusSquareFill className="w-6 h-6" />
+}
