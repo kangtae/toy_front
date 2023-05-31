e2b45830ae71f329d18763d0011e18cbbf88f2b9
kangtaewan <bras1717@naver.com>
2023-05-31 22:42:49 +0900

    새니티 설정 변경

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,35 +1,43 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google"
+import { addUser } from '@/service/user';
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 export const authOptions = {
 	// Configure one or more authentication providers
 	providers: [
 		GoogleProvider({
-			clientId: process.env.GOOGLE_OAUTH_ID || "",
-			clientSecret: process.env.GOOGLE_OAUTH_SECRET || "",
+			clientId: process.env.GOOGLE_OAUTH_ID || '',
+			clientSecret: process.env.GOOGLE_OAUTH_SECRET || '',
 		}),
 		// ...add more providers here
 	],
 	callbacks: {
-		async signIn ({ user: {id, name, image, email} }) {
-			if(!email) {
+		async signIn({ user: { id, name, image, email } }) {
+			if (!email) {
 				return false;
 			}
-			addUser({id, name: name || "", image, email, username: email.split("@")[0]})
+			addUser({
+				id,
+				name: name || '',
+				image,
+				email,
+				username: email.split('@')[0],
+			});
 			return true;
 		},
-		async session ( { session }) {
+		async session({ session }) {
 			console.log(session);
 			const user = session?.user;
 			if (user) {
 				session.user = {
 					...user,
-					username: user.email?.split("@")[0] || "",
-				}
+					username: user.email?.split('@')[0] || '',
+				};
 			}
-		}
+			return session;
+		},
 	},
 	pages: {
 		signIn: '/auth/signin',
 	},
-}
-export default NextAuth(authOptions)
+};
+export default NextAuth(authOptions);
--- src/service/user.ts
+++ src/service/user.ts
@@ -1,17 +1,17 @@
-import { client } from "./sanity";
+import { client } from './sanity';
 
-type OAuthUser ={
+type OAuthUser = {
 	id: string;
 	email: string;
 	name: string;
 	username: string;
 	image?: string | null;
-}
+};
 
-export async function addUser({id, username, email, name, image}: OAuthUser) {
+export async function addUser({ id, username, email, name, image }: OAuthUser) {
 	return client.createIfNotExists({
 		_id: id,
-		_type: "user",
+		_type: 'user',
 		username,
 		email,
 		name,
