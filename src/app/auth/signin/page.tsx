c466a1c1d1b7a67c3eedd59be9679c0d5c97f55a
kangtaewan <bras1717@naver.com>
2023-07-11 23:29:52 +0900

    브랜치 변경 및 로그인 마크업

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -22,7 +22,19 @@ export default async function SignPage({
   const providers = (await getProviders()) ?? {};
 
   return (
-    <section className='flex justify-center mt-24'>
+    <section className='flex justify-center column flex-col	 mt-24'>
+	    <h1>REDET</h1>
+	    <div>
+		    <h2>아이디& 휴대폰 번호</h2>
+		    <input type="text" placeholder="아이디를 입력하시기 바랍니다"/>
+	    </div>
+	    <div>
+		    <h2>비밀번호</h2>
+		    <input type="text" placeholder="비밀번호를 입력하시기 바랍니다"/>
+	    </div>
+			<div>
+				<button>로그인</button>
+			</div>
       <Signin providers={providers} callbackUrl={callbackUrl ?? '/'} />
     </section>
   );
